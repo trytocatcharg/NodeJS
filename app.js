219c5e2cdd7afa6b7f38f61ded64401aa65cf1e1
HDD_REPO <trytocatch.arg@gmail.com>
2018-08-24 20:20:12 +0200

    .

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express();
 const configRoute = require('./routes/config'); // Imports routes
 app.use('/api', configRoute);
 
-let port = normalizePort(process.env.PORT || 1234);
+///let port = normalizePort(process.env.PORT || 1234);
+let port = process.env.PORT;
+if (process.env.PORT==undefined) {
+    port=1234;
+}
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
