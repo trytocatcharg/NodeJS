773c4bd9aaa38a57c79eee6a1cb09e77e3042c9d
HDD_REPO <trytocatch.arg@gmail.com>
2018-08-26 03:13:15 +0200

    .

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
