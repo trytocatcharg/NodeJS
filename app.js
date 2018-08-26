9fea387c2ac8900c1352515c614bc8af327e9de3
HDD_REPO <trytocatch.arg@gmail.com>
2018-08-26 03:12:57 +0200

    .

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const configValues = require('./config-values')
 // initialize our express app
 const app = express();
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
 const configRoute = require('./routes/config'); // Imports routes
 const tradeRoute = require('./routes/trade'); // Imports routes
 app.use('/api', configRoute);
 app.use('/api', tradeRoute);
+app.use(allowCrossDomain);
 
 ///let port = normalizePort(process.env.PORT || 1234);
 let port =configValues.port;
