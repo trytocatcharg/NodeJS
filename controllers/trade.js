9798794d6f827bbcb07b7a40e97b6ab4d367aa19
HDD_REPO <trytocatch.arg@gmail.com>
2018-09-02 16:04:45 +0200

    .

--- controllers/trade.js
+++ controllers/trade.js
@@ -11,64 +11,52 @@ var moment = require('moment');
 
 
 //Trae todos los registros de Trades
-exports.getAll=function(req,res){
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("trading_db");
-           dbo.collection("Trades").find().toArray(function(err, result) {
-            console.log(result);
-            if (!result) return res.status(500).send({message: 'internal error'})
-            if (err) throw err;
-            db.close();
+// exports.getAll=function(req,res){
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("trading_db");
+//            dbo.collection("Trades").find().toArray(function(err, result) {
+//             console.log(result);
+//             if (!result) return res.status(500).send({message: 'internal error'})
+//             if (err) throw err;
+//             db.close();
             
-            res.status(200).send(result);
-        });
-    });
-}
+//             res.status(200).send(result);
+//         });
+//     });
+// }
 
-exports.getByYear=function(req,res){
-    var year =parseInt(req.params.year);
+exports.getFilter=function(req,res){
+    
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("trading_db");
-        var filter = {year:year};
-    
-    
-        console.log(filter);
-        dbo.collection("Trades").find(filter).toArray(function(err, result) {
-            console.log(result);
-            if (!result) return res.status(404).send({message: 'not found'})
-            if (err) throw err;
-            db.close();
-            res.status(200).send(result);
-        });
-    });
-}
-
-//Trae resultados de la semana. Se le envia una fecha calcula que semana es y devuelve resultados
-exports.getByWeek=function(req,res){
-    /*The way to avoid issues of that sort is to include the strict mode Boolean argument in the constructor invocation. A value of true specifies that the format and input must match exactly, including delimiters:
-    moment('2016 is a date', 'YYYY-MM-DD', true);  //no longer valid!*/
-    var isValid=moment(String(req.params.date), "YYYY-MM-DD",true).isValid();
-    if (!isValid){
-        return res.status(500).send({message: 'internal error'})
-    }   
-    var date = Date.parse(req.params.date);
-    var year=parseInt(moment(req.params.date).format("YYYY")); 
-    var marketType = String(req.params.type); //que tipo de mercado esta consultando... si viene vacio es todos
-    var numberOfWeek=common.getWeekNumber(date);
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("trading_db");
-        var filter = {weekOfYear:numberOfWeek, year:year};
+        var filter = {};
+       console.log(req.query.date);
+       console.log(req.query.year);
+        if(req.query.date){
+            var isValid=moment(String(req.query.date), "YYYY-MM-DD",true).isValid();
+            if (!isValid){
+                return res.status(500).send({message: 'internal error'})
+            }   
+            var date = Date.parse(req.query.date);
+            var year=parseInt(moment(req.query.date).format("YYYY")); 
+            var marketType = String(req.query.type); //que tipo de mercado esta consultando... si viene vacio es todos
+            var numberOfWeek=common.getWeekNumber(date);
+            var filter = {weekOfYear:numberOfWeek, year:year};
         
-        if (typeof(marketTyp) != "undefined"){
-            //si no viene nada traigo sin filtrar por el tipo de mercado
-            filter = {weekOfYear:numberOfWeek, year:year,type:marketType.toUpperCase()};
+            if (typeof(marketTyp) != "undefined"){
+                //si no viene nada traigo sin filtrar por el tipo de mercado
+                filter = {weekOfYear:numberOfWeek, year:year,type:marketType.toUpperCase()};
+            }
+        }else{
+            
+        if(req.query.year){
+            var year =parseInt(req.query.year);
+            filter = {year:year};
+            }
         }
-
-        console.log(filter);
+    console.log(filter);
         dbo.collection("Trades").find(filter).toArray(function(err, result) {
             console.log(result);
             if (!result) return res.status(404).send({message: 'not found'})
@@ -76,5 +64,66 @@ exports.getByWeek=function(req,res){
             db.close();
             res.status(200).send(result);
         });
+    
     });
 }
+
+// exports.getByYear=function(req,res){
+//     var year =parseInt(req.params.year);
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("trading_db");
+//         var filter = {year:year};
+    
+//         Trade.find(filter,function(err, result) {
+//             console.log(result);
+//             if (!result) return res.status(404).send({message: 'not found'})
+//             if (err) throw err;
+//             db.close();
+//             res.status(200).send(result);
+//         });
+    
+//         // console.log(filter);
+//         // dbo.collection("Trades").find(filter).toArray(function(err, result) {
+//         //     console.log(result);
+//         //     if (!result) return res.status(404).send({message: 'not found'})
+//         //     if (err) throw err;
+//         //     db.close();
+//         //     res.status(200).send(result);
+//         // });
+//     });
+// }
+
+// //Trae resultados de la semana. Se le envia una fecha calcula que semana es y devuelve resultados
+// exports.getByWeek=function(req,res){
+//     /*The way to avoid issues of that sort is to include the strict mode Boolean argument in the constructor invocation. A value of true specifies that the format and input must match exactly, including delimiters:
+//     moment('2016 is a date', 'YYYY-MM-DD', true);  //no longer valid!*/
+//     var isValid=moment(String(req.params.date), "YYYY-MM-DD",true).isValid();
+//     if (!isValid){
+//         return res.status(500).send({message: 'internal error'})
+//     }   
+//     var date = Date.parse(req.params.date);
+//     var year=parseInt(moment(req.params.date).format("YYYY")); 
+//     var marketType = String(req.params.type); //que tipo de mercado esta consultando... si viene vacio es todos
+//     var numberOfWeek=common.getWeekNumber(date);
+
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("trading_db");
+//         var filter = {weekOfYear:numberOfWeek, year:year};
+        
+//         if (typeof(marketTyp) != "undefined"){
+//             //si no viene nada traigo sin filtrar por el tipo de mercado
+//             filter = {weekOfYear:numberOfWeek, year:year,type:marketType.toUpperCase()};
+//         }
+
+//         console.log(filter);
+//         dbo.collection("Trades").find(filter).toArray(function(err, result) {
+//             console.log(result);
+//             if (!result) return res.status(404).send({message: 'not found'})
+//             if (err) throw err;
+//             db.close();
+//             res.status(200).send(result);
+//         });
+//     });
+// }
--- routes/trade.js
+++ routes/trade.js
@@ -5,9 +5,10 @@ var TradeController = require('../controllers/trade');
 
 // a simple test url to check that all of our files are communicating correctly.
 // api.get('/test', ConfigController.test);
-api.get('/trade/getByWeek/:date', TradeController.getByWeek);
-api.get('/trade/getByYear/:year', TradeController.getByYear);
-api.get('/trade/', TradeController.getAll);
+// api.get('/trade/getByWeek/:date', TradeController.getByWeek);
+// api.get('/trade/getByYear/:year', TradeController.getByYear);
+api.get('/trades/', TradeController.getFilter);
+//api.get('/trade/', TradeController.getAll);
 
 
 module.exports = api;
